7af756e5848be47ea9d452270cf3163521b49a9d
Arthur Brandao <abrandao@nextoo.fr>
2019-04-12 10:59:01 +0200

    Autorisation CrossOrigin

--- src/main/java/nxt/weather/controller/WeatherController.java
+++ src/main/java/nxt/weather/controller/WeatherController.java
@@ -6,6 +6,7 @@ import nxt.weather.controller.dto.HumidityDto;
 import nxt.weather.controller.dto.ReturnDto;
 import nxt.weather.service.WeatherInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/meteo/")
+@CrossOrigin
 public class WeatherController {
     
     /**
