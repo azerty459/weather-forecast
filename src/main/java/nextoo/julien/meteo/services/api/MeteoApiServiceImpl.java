09c5ceb26dff2c03bea7248ae67b3189e6b1387e
julien <jdutrieux@nextoo.fr>
2019-04-12 08:32:00 +0200

    Normalisation du nom de la ville recherchée

--- src/main/java/nextoo/julien/meteo/services/api/MeteoApiServiceImpl.java
+++ src/main/java/nextoo/julien/meteo/services/api/MeteoApiServiceImpl.java
@@ -1,7 +1,9 @@
 package nextoo.julien.meteo.services.api;
 
 import java.io.IOException;
+import java.text.Normalizer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,19 +13,33 @@ import nextoo.julien.meteo.services.api.dto.MeteoDto;
 public class MeteoApiServiceImpl implements MeteoApiService {
 
 //	@Autowired
-//	private RestTemplate restTemplate;
+	private RestTemplate restTemplate = getRestTemplate();
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	private static final String RESSOURCE_URL = "https://www.prevision-meteo.ch/services/json/";
 
 	public MeteoDto getMeteo(String ville) throws IOException {
 		
-		String villeRecherche = ville.replaceAll(" ", "-").replaceAll("'", "-");
+		//String villeRecherche = ville.replaceAll(" ", "-").replaceAll("'", "-");
 
-		RestTemplate restTemplate = new RestTemplate();
+		//RestTemplate restTemplate = new RestTemplate();
+		
+		String villeRecherche = getNomVilleNormalise(ville);
 
 		MeteoDto meteo = restTemplate.getForObject(RESSOURCE_URL + villeRecherche, MeteoDto.class);
 
 		return meteo;
+	}
+	
+	public static String getNomVilleNormalise(String ville) {
+		
+		String vr = ville.replaceAll(" ", "-").replaceAll("'", "-");
+		
+		  return Normalizer.normalize(vr, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
 	}
 
 }
